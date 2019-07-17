bc72972408408f0f4f8ed276b94d20cdafc5cd82
Maximus06 <o.maj@free.fr>
2019-07-17 20:58:15 +0200

    Updates

---
+++ precache-manifest.d07de018825b23f380d29b21f91c9338.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdc5d483022f5d482826920615560d39",
+    "url": "/monsters-robots/index.html"
+  },
+  {
+    "revision": "d49bb080ad71f4081c99",
+    "url": "/monsters-robots/static/css/main.38da9432.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-robots/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "d49bb080ad71f4081c99",
+    "url": "/monsters-robots/static/js/main.f5129ecc.chunk.js"
+  },
+  {
+    "revision": "56770b577e55ab03595f",
+    "url": "/monsters-robots/static/js/runtime~main.86a134b2.js"
+  }
+]);
